3bfd0a7af6d8153c2a0edac7fa7f8a1df6408d91
Nils Schweinsberg <mail@nils.cc>
2017-12-04 22:57:54 +0100

    Comments

--- 04.hs
+++ 04.hs
@@ -1,10 +1,18 @@
-import Control.Arrow
-import Data.List
+---------------------------------------------------------------------------
+--
+--       2017        * A D V E N T * O F * C O D E *            DAY4
+--
+--                                starring
+--
+import                         Control.Arrow
+import                            Data.List
+--
+-------------------- P A R T  O N E ----- P A R T  T W O ------------------
 
-valid = uncurry (==) . ((nub.sort) &&& sort)
+main =                              print
+ .(                           length *** length                           )
+ .(             filter (valid.words) &&& filter (valid.map sort.words)    )
+ .                                  lines
+ =<< readFile                      "04.in"
 
-main =                            print
- .(                         length *** length                           )
- .(           filter (valid.words) &&& filter (valid.map sort.words)    )
- .                                lines
- =<< readFile                    "04.in"
+valid = uncurry (==) .   ((nub.sort) &&& sort)
