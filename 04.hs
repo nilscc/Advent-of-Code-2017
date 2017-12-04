fee40bb75127381f959bf18c471786f43c448fb0
Nils Schweinsberg <mail@nils.cc>
2017-12-04 22:42:17 +0100

    One-line 'valid'

--- 04.hs
+++ 04.hs
@@ -1,11 +1,10 @@
 import Control.Arrow
 import Data.List
 
-valid = uncurry (==)
-  .(                      (nub.sort) &&& sort                             )
+valid = uncurry (==) . ((nub.sort) &&& sort)
 
-main =                              print
- .(                           length *** length                           )
- .(             filter (valid.words) &&& filter (valid.map sort.words)    )
- .                                  lines
- =<< readFile                      "04.in"
+main =                            print
+ .(                         length *** length                           )
+ .(           filter (valid.words) &&& filter (valid.map sort.words)    )
+ .                                lines
+ =<< readFile                    "04.in"
