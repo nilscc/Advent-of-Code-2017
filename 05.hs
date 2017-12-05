30c46f069f5fba0cbf0f129262e617d4c4e7665e
Nils Schweinsberg <mail@nils.cc>
2017-12-05 21:09:57 +0100

    Small cleanup

--- 05.hs
+++ 05.hs
@@ -1,34 +1,24 @@
 import qualified Data.Vector.Mutable as V
 import Control.Monad
 
-getData :: IO [Int]
-getData = (map read . lines) <$> readFile "05.in"
-
 main :: IO ()
 main = do
-  d <- getData
-  part1 d
-  part2 d
+  d <- (map read . lines) <$> readFile "05.in"
+  putStr "Part One: " >> part1 d
+  putStr "Part Two: " >> part2 d
 
-part1 = solve (const (+1))
-part2 = solve (\o -> if o >= 3 then subtract 1 else (+1))
+part1 = solve (+1)
+part2 = solve (\o -> if o >= 3 then o-1 else o+1)
 
 solve f d = do
-
-  let l = length d
-
-  -- copy to mutable vector
   v <- V.new l
-  forM_ (zip [0..] d) $ \(i, val) ->
-    V.write v i val
-
-  -- solve problem
-  let go i s
-        | i >= l = print s
-        | otherwise = do
-          -- get offset
-          o <- V.read v i
-          V.modify v (f o) i
-          go (i+o) (s+1)
-
-  go 0 0
+  forM_ (zip [0..] d) $ uncurry (V.write v)
+  go v 0 0
+ where
+  l = length d
+  go v i s
+    | i < 0 || i >= l = print s
+    | otherwise = do
+      o <- V.read v i
+      V.write v i (f o)
+      go v (i+o) (s+1)
